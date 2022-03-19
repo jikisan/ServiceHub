1e9bee42c3167d6486948e91f461f63fbefce3d4
jikisan <santernakyle@gmail.com>
2022-03-19 16:29:27 +0800

    Setup firebase and layout referrences

--- app/src/main/java/com/example/servicehub/sign_up_page.java
+++ app/src/main/java/com/example/servicehub/sign_up_page.java
@@ -5,11 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class sign_up_page extends AppCompatActivity {
 
+    EditText et_firstName, et_lastName, et_contactNumber, et_email, et_password_signup,
+            et_confirmPassword;
     TextView tv_signIn;
+    Button btn_signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +22,12 @@ public class sign_up_page extends AppCompatActivity {
         setContentView(R.layout.sign_up_page);
 
         setRef();
+        buttonClickListener();
 
+
+    }
+
+    private void buttonClickListener() {
         tv_signIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -25,11 +35,18 @@ public class sign_up_page extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-
     }
 
     private void setRef() {
+
         tv_signIn = findViewById(R.id.tv_signIn);
+        et_firstName = findViewById(R.id.et_firstName);
+        et_lastName = findViewById(R.id.et_lastName);
+        et_contactNumber = findViewById(R.id.et_contactNumber);
+        et_email = findViewById(R.id.et_email);
+        et_password_signup = findViewById(R.id.et_password_signup);
+        et_confirmPassword = findViewById(R.id.et_confirmPassword);
+        btn_signUp = findViewById(R.id.btn_signUp);
+
     }
 }
