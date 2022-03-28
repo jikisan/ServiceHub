b2e354a6c878c45827e10aa3c98c865d1d7a895e
jikisan <santernakyle@gmail.com>
2022-03-29 00:43:08 +0800

    Setup upload photo function

--- app/src/main/java/com/example/servicehub/add_project_page.java
+++ app/src/main/java/com/example/servicehub/add_project_page.java
@@ -1,9 +1,19 @@
 package com.example.servicehub;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -11,6 +21,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -19,19 +30,25 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.io.InputStream;
+import java.util.Locale;
 
 public class add_project_page extends AppCompatActivity {
     ImageView iv_messageBtn, iv_notificationBtn, iv_homeBtn, iv_accountBtn,
-            iv_moreBtn, iv_decreaseSlot, iv_increaseSlot;
-    EditText et_projectName, et_address, et_price, et_timeSlot, et_specialInstruction;
+            iv_moreBtn, iv_decreaseSlot, iv_increaseSlot, iv_projectImage;
+    EditText et_projectName, et_address, et_price, et_specialInstruction;
     Button btn_pickTime, btn_save;
-    TextView tv_uploadPhoto, tv_slotCount;
+    TextView tv_uploadPhoto, tv_slotCount, tv_timeSlot;
 
+    int hour, minute;
     int slotCount = 1;
     String slotCountText;
 
     FirebaseAuth fAuth;
-    private FirebaseUser user;
+    private FirebaseUser project;
     private DatabaseReference projectDatabase;
 
     @Override
@@ -40,7 +57,7 @@ public class add_project_page extends AppCompatActivity {
         setContentView(R.layout.add_project_page);
 
 
-        user = FirebaseAuth.getInstance().getCurrentUser();
+        project = FirebaseAuth.getInstance().getCurrentUser();
         projectDatabase = FirebaseDatabase.getInstance().getReference(Projects.class.getSimpleName());
 
         setRef();
@@ -56,31 +73,110 @@ public class add_project_page extends AppCompatActivity {
                 addProj();
             }
         });
+
+        btn_pickTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int i, int i1) {
+                        hour = i;
+                        minute = i1;
+
+                        tv_timeSlot.setText(String.format(Locale.getDefault(), "%02d:%02d", hour, minute));
+
+                    }
+                };
+
+                int style = TimePickerDialog.THEME_HOLO_DARK;
+
+                TimePickerDialog timePickerDialog = new TimePickerDialog(add_project_page.this, style, onTimeSetListener, hour, minute, true);
+                timePickerDialog.setTitle("Select Time");
+                timePickerDialog.show();
+            }
+        });
+
+        tv_uploadPhoto.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View view) {
+                boolean pick = true;
+                if (pick == true){
+                    if(!checkCameraPermission()){
+                        requestCameraPermission();
+                    }else
+                        PickImage();
+
+                }else{
+                    if(!checkStoragePermission()){
+                        requestStoragePermission();
+                    }else
+                        PickImage();
+                }
+            }
+        });
+    }
+
+    private void PickImage() {
+        CropImage.activity().start(this);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void requestStoragePermission() {
+        requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 100);
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void requestCameraPermission() {
+        requestPermissions(new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 100);
+    }
+
+    private boolean checkStoragePermission() {
+        boolean res2 = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED;
+        return res2;
+    }
+
+    private boolean checkCameraPermission() {
+        boolean res1 = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)== PackageManager.PERMISSION_GRANTED;
+        boolean res2 = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED;
+        return res1 && res2;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                Uri resultUri = result.getUri();
+
+                try{
+                    InputStream stream = getContentResolver().openInputStream(resultUri);
+                    Bitmap bitmap = BitmapFactory.decodeStream(stream);
+                    iv_projectImage.setImageBitmap(bitmap);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
     }
 
     private void addProj() {
         String projName = et_projectName.getText().toString();
         String projAddress = et_address.getText().toString();
         String price = et_price.getText().toString();
-        String projTimeSlot = et_timeSlot.getText().toString();
+        String projTimeSlot = tv_timeSlot.getText().toString();
         String projTimeSlotCount = tv_slotCount.getText().toString();
         String projInstruction = et_specialInstruction.getText().toString();;
 
-        String userID = user.getUid();
+        String userID = project.getUid();
 
         Projects projects = new Projects(projName, projAddress, price, projTimeSlot, projTimeSlotCount, projInstruction, userID);
-
-//        projectDatabase.child(FirebaseAuth.getInstance().getCurrentUser().getUid())
-//                .setValue(projects).addOnCompleteListener(new OnCompleteListener<Void>() {
-//            @Override
-//            public void onComplete(@NonNull Task<Void> task) {
-//                if (task.isSuccessful()) {
-//                    Toast.makeText(add_project_page.this, "Project Added", Toast.LENGTH_LONG).show();
-//                } else {
-//                    Toast.makeText(add_project_page.this, "Failed " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-//                }
-//            }
-//        });
 
         projectDatabase.push().setValue(projects).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
@@ -93,7 +189,6 @@ public class add_project_page extends AppCompatActivity {
             }
         });
     }
-
     private void adjustSlot() {
 
         slotCountText = String.valueOf(slotCount);
@@ -181,9 +276,10 @@ public class add_project_page extends AppCompatActivity {
         iv_moreBtn = findViewById(R.id.iv_moreBtn);
         iv_decreaseSlot = findViewById(R.id.iv_decreaseSlot);
         iv_increaseSlot = findViewById(R.id.iv_increaseSlot);
+        iv_projectImage = findViewById(R.id.iv_projectImage);
         et_projectName = findViewById(R.id.et_projectName);
         et_price = findViewById(R.id.et_price);
-        et_timeSlot = findViewById(R.id.et_timeSlot);
+        tv_timeSlot = findViewById(R.id.tv_timeSlot);
         et_address = findViewById(R.id.et_address);
         et_specialInstruction = findViewById(R.id.et_specialInstruction);
         btn_save = findViewById(R.id.btn_save);
